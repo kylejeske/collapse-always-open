68eaac7ffcc3ff500a37cdf2d637ec6d425f8a90
Kyle Jeske <kylejeske@gmail.com>
2013-01-02 06:53:18 -0600

    added jquery plugin to code

---
+++ jquery.collapsealwaysopen.js
@@ -0,0 +1,33 @@
+(function($){
+	/**
+	 * Usage: $("#collapse_group).collapseAlwaysOpen();
+	 * Will cause the active collapse (accordion) group to remain open
+	 * as it checks for an open group item, if none exists, the item that was just
+	 * closed will re-open
+	 */
+	$.fn.collapseAlwaysOpen = function() {
+		var master_id = "#" + $(this).attr('id');
+		$(master_id).collapse({
+			toggle: false
+		});
+		function checkGroup(id, target) {
+			var open = false;
+			var obj = jQuery(id);
+			obj.find(".accordion-group").each(function(i, item){
+				jQuery(item).find(".collapse").each(function(i, group_item){
+					if (jQuery(group_item).hasClass("in")) {
+						open = true;
+					}
+				});
+			});
+			if (!open) {
+				$("#"+target).collapse("toggle");
+			}
+		}
+		$(master_id).find(".accordion-group").live("hidden", function(e){
+			var obj = $(this);
+			var target_id = $(e.target).attr('id');
+			checkGroup(master_id, target_id);
+		});
+	};
+})(jQuery);
