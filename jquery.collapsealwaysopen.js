dd01bfc1c40d14818a61a2b017eec89cfbdb3f3d
Kyle Jeske <kylejeske@gmail.com>
2013-01-03 16:32:31 -0600

    Added the ability to do namespaces so you can have multiple 'always open' on one page

--- jquery.collapsealwaysopen.js
+++ jquery.collapsealwaysopen.js
@@ -1,33 +1,41 @@
 (function($){
 	/**
-	 * Usage: $("#collapse_group).collapseAlwaysOpen();
+	 * Usage: $("#collapse_group).collapseAlwaysOpen({ namespace: '.groupA' });
 	 * Will cause the active collapse (accordion) group to remain open
 	 * as it checks for an open group item, if none exists, the item that was just
 	 * closed will re-open
 	 */
-	$.fn.collapseAlwaysOpen = function() {
+	$.fn.collapseAlwaysOpen = function(options) {
+		if (!options) {
+			var options = {
+				'namespace': false
+			};
+		}
+		if (!options.namespace) {
+			options.namespace = "";
+		}	
 		var master_id = "#" + $(this).attr('id');
 		$(master_id).collapse({
 			toggle: false
 		});
-		function checkGroup(id, target) {
+		function checkGroup(id, target, namespace) {
 			var open = false;
-			var obj = $(id);
-			obj.find(".accordion-group").each(function(i, item){
-				$(item).find(".collapse").each(function(i, group_item){
-					if ($(group_item).hasClass("in")) {
-						open = true;
-					}
-				});
+			var obj = jQuery(id);
+
+			$(".accordion-group"+namespace).children().each(function(i, obj){
+				var that = $(this);
+				if(that.hasClass("collapse") && that.hasClass("in")){ 
+					open = true;
+				}
 			});
 			if (!open) {
 				$("#"+target).collapse("toggle");
 			}
 		}
-		$(master_id).find(".accordion-group").live("hidden", function(e){
+		$(master_id).find(".accordion-group"+options.namespace).live("hidden", function(e){
 			var obj = $(this);
 			var target_id = $(e.target).attr('id');
-			checkGroup(master_id, target_id);
+			checkGroup(master_id, target_id, options.namespace);
 		});
 	};
 })(jQuery);
